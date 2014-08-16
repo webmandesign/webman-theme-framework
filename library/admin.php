916bf06e2d015265740c080aa06fa901d0841f99
webmandesign <webmandesigneu@gmail.com>
2014-08-16 12:31:26 +0200

    Addition to update 3.1

--- library/admin.php
+++ library/admin.php
@@ -102,7 +102,7 @@
 			add_filter( 'login_headertitle', 'wm_login_headertitle' );
 			add_filter( 'login_headerurl', 'wm_login_headerurl' );
 		//Admin customization
-			if ( is_admin() ) {
+			if ( is_admin() && apply_filters( 'wmhook_admin_modifications_enabled', true ) ) {
 				add_filter( 'admin_footer_text', 'wm_admin_footer' );
 				add_filter( 'manage_post_posts_columns', 'wm_post_columns_register' );
 				add_filter( 'manage_post_posts_custom_column', 'wm_post_columns_render', 10 );
