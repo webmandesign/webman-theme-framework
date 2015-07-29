fa447b050b6fec2f1a24688ee87f78d4a86363ba
webmandesign <webmandesigneu@gmail.com>
2015-07-29 10:59:19 +0200

    5.0.pre.18
    Fixing custom HTML customizer control output.
    Improving range customizer control styles.

--- complex/lib/inc/classes/controls/class-control-html.php
+++ complex/lib/inc/classes/controls/class-control-html.php
@@ -22,15 +22,17 @@ class {%= prefix_class %}_Control_HTML extends WP_Customize_Control {
 	public $content = '';
 
 	public function render_content() {
-		if ( isset( $this->label ) ) {
+		if ( isset( $this->label ) && ! empty( $this->label ) ) {
 			echo '<span class="customize-control-title">' . $this->label . '</span>';
 		}
 
 		if ( isset( $this->content ) ) {
 			echo $this->content;
+		} else {
+			esc_html_e( 'Please set the `content` parameter for the HTML control.', '{%= text_domain %}' );
 		}
 
-		if ( isset( $this->description ) ) {
+		if ( isset( $this->description ) && ! empty( $this->description ) ) {
 			echo '<span class="description customize-control-description">' . $this->description . '</span>';
 		}
 	}
