a310fe1da0942da6e1e3ebad0e51fb938edefcd8
Oliver Juhas | WebMan Design <webmandesigneu@gmail.com>
2018-02-22 19:26:14 +0100

    2.7.0

--- library/init.php
+++ library/init.php
@@ -11,7 +11,7 @@
  *
  * @package    WebMan WordPress Theme Framework
  * @copyright  WebMan Design, Oliver Juhas
- * @license    GPL-3.0, http://www.gnu.org/licenses/gpl-3.0.html *
+ * @license    GPL-3.0, http://www.gnu.org/licenses/gpl-3.0.html
  * @version    2.7.0
  *
  * Used development prefixes:
